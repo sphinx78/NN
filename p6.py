f8a02f60ce04ab65a9ef5f860b2aaa8c8a034576
Shubham Kayastha <sk5078e@gmail.com>
2026-05-09 17:37:29 +0545

    Delete p6.py

--- p6.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-input=[[4.8, 1.21, 2.385], 
-       [8.9, -1.81, 0.2], 
-       [1.41, 1.051, 0.026]]
-print(max(input))
-# print(np.exp(input))
-# print(np.sum(input,axis=1,keepdims=True))
-# exp=np.exp(input)
-# print( exp/ np.sum(exp,axis=1,keepdims=True))
