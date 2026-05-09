185791591840d021b0c86de6be625b5b5a33a83b
Shubham Kayastha <sk5078e@gmail.com>
2026-05-09 18:43:37 +0545

    Added backward functions

--- main.py
+++ main.py
@@ -1,37 +1,6 @@
 import numpy as np
-# inputs =[1,2,3,2.5]
-# weights = [[0.2,0.8,-0.5,1.0],
-#               [0.5,-0.91,0.26,-0.5],
-#                 [-0.26,-0.27,0.17,0.87]]
-# bias=[2,3,0.5]
-
-# outputs = []
-# for n_weights, n_bias in zip(weights,bias):
-#     output=0
-#     for n_inputs,n_weights in zip(inputs, n_weights):
-#         output += n_inputs*n_weights
-#     output+=n_bias
-#     outputs.append(output)
-
-# print(outputs)
 
 
-# output = np.dot(weights, inputs) + bias
-# print(output)
-
-# inputs = [[1.0, 2.0, 3.0, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8] ]
-# weights = [[0.2, 0.8, -0.5, 1.0],[ 10.5, -0.91, 0.26, -0.5],[-0.26, -0.27, 0.17, 0.87] ]
-# biases = [2.0, 3.0, 0.5]
-
-# weights2 = [[ 0.1 ,- 0.14 , 0.5 ],
-# [ - 0.5 , 0.12 ,- 0.33 ],
-# [ - 0.44 , 0.73 ,- 0.13 ]]
-# biases2 = [ - 1 , 2 ,- 0.5 ]
-# output=np.dot(inputs,np.array(weights).T) +biases
-# print(output)
-# all
-# output_final=np.dot(output,weights2) + biases2
-# print(output_final)
 def create_data(points, classes):
     X = np.zeros( (points*classes, 2))
     y = np.zeros (points*classes, dtype='uint8') 
@@ -44,7 +13,6 @@ def create_data(points, classes):
         y[ix] = class_number 
     return X, y
 
-# X = [[1.0, 2.0, 3.0, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8] ]
 class Loss:
     def calculate(self, output,y):
         sample_losses=self.forward(output,y)
@@ -63,40 +31,96 @@ class CategoricalCrossEntropy(Loss):
 
         neg_log_likelihood=-np.log(correct_confidence)
         return neg_log_likelihood
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        labels = len(dvalues[0])
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+        self.dinputs = -y_true / dvalues
+        self.dinputs = self.dinputs / samples
 
+class Activation_SoftmaxLoss_CategoricalCrossentropy():
+    def __init__(self):
+        self.activation = Activation_Softmax()
+        self.loss = CategoricalCrossEntropy()
+    
+
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(self.output, y_true)
+    
+  
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
 
 class Activation_ReLU:
-    def forward(self,inputs):
-        self.output=np.maximum(0,inputs)
-class Activation_softmax:
-    def forward(self,inputs):
-        self.output=(np.exp(inputs-np.max(inputs,axis=1,keepdims=True)))/np.sum((np.exp(inputs-np.max(inputs,axis=1,keepdims=True))),axis=1,keepdims=True)
-class Layer_dense:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        self.output = (np.exp(inputs - np.max(inputs, axis=1, keepdims=True))) / np.sum((np.exp(inputs - np.max(inputs, axis=1, keepdims=True))), axis=1, keepdims=True)
+    
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            single_output = single_output.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+class Layer_Dense:
     def __init__(self, input_n, neurons_n):
-        self.weights= 0.1*np.random.rand(input_n,neurons_n)
-        self.biases=np.zeros((1, neurons_n))
-    def forward(self,inputs):
-        self.output=np.dot(inputs,self.weights)+self.biases
+        self.weights = 0.1 * np.random.randn(input_n, neurons_n)
+        self.biases = np.zeros((1, neurons_n))
+    
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+    
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 
-X,y=create_data(100,3)
-layer1=Layer_dense(2,3)
-layer1.forward(X)
-activation1=Activation_ReLU()
-activation1.forward(layer1.output)
 
+X, y = create_data(100, 3)
 
-layer2=Layer_dense(3,3)
-layer2.forward(activation1.output)
-activation2=Activation_softmax()
-activation2.forward(layer2.output)
-output=activation2.output
-print(output[:5])
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3, 3)
+loss_activation = Activation_SoftmaxLoss_CategoricalCrossentropy()
 
-Loss_function=CategoricalCrossEntropy()
-loss=Loss_function.calculate(output,y)
-print("Loss: ", loss)
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+loss = loss_activation.forward(dense2.output, y)
 
-predictions=np.argmax(output,axis=1)
-accuracy=np.mean(predictions==y)
-print("accuracy=", accuracy)
+print(loss_activation.output[:5])
+print('loss:', loss)
+
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+accuracy = np.mean(predictions == y)
+print('acc:', accuracy)
+
+loss_activation.backward(loss_activation.output, y)
+dense2.backward(loss_activation.dinputs)
+activation1.backward(dense2.dinputs)
+dense1.backward(activation1.dinputs)
+
+print(dense1.dweights)
+print(dense1.dbiases)
+print(dense2.dweights)
+print(dense2.dbiases)
